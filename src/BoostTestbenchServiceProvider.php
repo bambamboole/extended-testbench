bf98bd4d3555db196639b02b5cb3109d40686a73
Manuel Christlieb <manuel.christlieb@xentral.com>
2026-08-02 00:39:05 +0200

    chore: scaffold package

---
+++ src/BoostTestbenchServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bambamboole\BoostTestbench;
+
+use Illuminate\Support\ServiceProvider;
+
+class BoostTestbenchServiceProvider extends ServiceProvider
+{
+    public function register(): void {}
+}
